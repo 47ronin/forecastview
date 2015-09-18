a2b725031707f791c9cdc954ffe0a1029f7eaf14
Glenn Batuyong <glenn@47ronin.com>
2015-09-18 11:03:53 -0700

    More cleanup, restoring angular-mocks to dev

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,8 +27,8 @@ module.exports = function(config) {
       'bower_components/moment/moment.js',
       'bower_components/angular-moment/angular-moment.js',
       'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
